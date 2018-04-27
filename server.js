55688c8d7eaaf1b73fedd2dff4113a507244425b
Ray Kao <ray@peopleandcode.com>
2018-04-27 17:37:35 -0400

    adding some authentication middleware

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const socketServer = require('socket.io')(3000);
+var jwt = require('jsonwebtoken');
+const tokenSecret = process.env.TOKEN_SECRET || "secret";
+
 
 socketServer.set('origins', 'http://localhost:8080');
 
 socketServer.use(function(socket, next){
-  // const userId = JSON.parse(atob(socket.request.headers.token));
-  console.log(socket.request.headers.token);
-  next();
+  jwt.verify(socket.request.headers.token, tokenSecret, (err, decodedToken) => {
+    if(err) {
+      next(new Error("Unauthorized"));
+    } else {
+      next();
+    }
+  });
 });
 
 socketServer.on('connection', function(session){
@@ -19,8 +26,4 @@ socketServer.on('connection', function(session){
 
 function atob(base64encodedString){
   return Buffer.from(base64encodedString, 'base64').toString();
-}
-
-function decodedJWT(jwt) {
-
 }
